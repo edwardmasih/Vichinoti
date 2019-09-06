81ffb1beffd696485707e730b1405011eba5a848
Edward Masih <masih.edward45@gmail.com>
2019-09-06 20:27:20 +0530

    removed List<Colors> with Colors

--- lib/model/places.dart
+++ lib/model/places.dart
@@ -1,51 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Places {
-
   final String name;
   final String imagePath;
+  final Color shadowColors;
   final String placeDescription;
-  final List<Color> colors;
 
-  Places({this.name, this.imagePath, this.placeDescription, this.colors});
-
+  Places({this.name, this.imagePath, this.shadowColors, this.placeDescription});
 }
 
 List places = [
-
   Places(
     name: "Mountains",
     imagePath: "assets/images/pic(3).jpg",
     placeDescription: "Something about Mountains",
-    colors: [Colors.brown, Colors.brown[200]],
+    shadowColors: Colors.brown,
   ),
   Places(
     name: "Beaches",
     imagePath: "assets/images/pic(2).jpg",
     placeDescription: "Something about Beaches",
-    colors: [Colors.brown, Colors.brown[200]],
+    shadowColors: Colors.brown,
   ),
   Places(
     name: "Forest",
     imagePath: "assets/images/pic(4).jpg",
     placeDescription: "Something about Forests",
-    colors: [Colors.brown, Colors.brown[200]],
+    shadowColors: Colors.brown,
   ),
   Places(
     name: "Desert",
     imagePath: "assets/images/pic(1).jpg",
     placeDescription: "Something about Deserts",
-    colors: [Colors.brown, Colors.brown[200]],
+    shadowColors: Colors.brown,
   ),
   Places(
     name: "Heaven",
     imagePath: "assets/images/pic(5).jpg",
     placeDescription: "Something about Heaven",
-    colors: [Colors.brown, Colors.brown[200]],
+    shadowColors: Colors.brown,
   ),
-
 ];
-
 
 //Color(0xffA37943)
 //Color(0xffcfa380)
